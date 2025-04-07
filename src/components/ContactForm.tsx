3ccb5906820fe7e0b1f0a589fa10407fbb80337d
ChristianCurtis06 <ctcurtis10@gmail.com>
2025-04-06 20:53:58 -0500

    Implemented Firestore to Contact Form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,21 +1,42 @@
 import { useState } from "react";
+import { db } from "../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
+
+interface ContactFormProps {
+    name: string;
+    email: string;
+    phone?: string;
+    message?: string;
+}
 
 const ContactForm: React.FC = () => {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const [contactForm, setContactForm] = useState<ContactFormProps>({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+    });
+    const [error, setError] = useState<string | null>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setContactForm((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-
-        setName("");
-        setEmail("");
-        setPhone("");
-        setMessage("");
-        alert("Form submitted! Thanks for reaching out!");
-    }
-
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [phone, setPhone] = useState<string>("");
-    const [message, setMessage] = useState<string>("");
+        try {
+            await addDoc(collection(db, "contact form submissions"), contactForm);
+            alert("Form submitted! Thanks for reaching out!");
+        } catch (error) {
+            setError(error as string);
+            alert("Error submitting the form. Please try again.");
+            console.error("Error submitting form: ", error);
+        }
+    };
 
     return (
         <section id="contact-me">
@@ -33,21 +54,23 @@ const ContactForm: React.FC = () => {
                         <fieldset>
                             <legend>Get in Touch</legend>
                             <label htmlFor="name">Name:</label>
-                            <input type="text" id="name" name="name" value={name} placeholder="Enter your full name (required)" onChange={(e) => setName(e?.target.value)} required />
+                            <input type="text" id="name" name="name" value={contactForm.name} placeholder="Enter your full name (required)" onChange={handleChange} required />
                             <br />
                             <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" name="email" value={email} placeholder="Enter your email (required)" onChange={(e) => setEmail(e?.target.value)} required />
+                            <input type="email" id="email" name="email" value={contactForm.email} placeholder="Enter your email (required)" onChange={handleChange} required />
                             <br />
                             <label htmlFor="phone">Phone Number:</label>
-                            <input type="tel" id="phone" name="phone" value={phone} pattern="[0-9]{10}" placeholder="Enter your phone number" onChange={(e) => setPhone(e?.target.value)} />
+                            <input type="tel" id="phone" name="phone" value={contactForm.phone} pattern="[0-9]{10}" placeholder="Enter your phone number" onChange={handleChange} />
                             <br />
                             <label htmlFor="message">Message:</label>
-                            <textarea id="message" name="message" value={message} rows={4} cols={50} placeholder="Enter your message here" onChange={(e) => setMessage(e?.target.value)} />
+                            <textarea id="message" name="message" value={contactForm.message} rows={4} cols={50} placeholder="Enter your message here" onChange={handleChange} />
                             <br />
                                     
                             <div role="alert" aria-live="assertive" id="error-message">
                                 <em>Please fill out all required fields.</em>
                             </div>
+                            {error && <p className="error">{error}</p>}
+                            <br />
                             <button type="submit">Submit</button>
                         </fieldset>
                     </form>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar: React.FC = () => {
     <nav>
 
       <ul id="navbar" className={interaction.isScrolled ? "scrolled" : ""}>
-        <img src="CC_Signature-white.png" alt="Christian Tanner Favicon" id="navbar-favicon" />
+        <img src="CC_Signature-white.png" alt="Christian Tanner Favicon" id="navbar-favicon" onClick={() => window.location.href = "/"} />
         <li className="navbar-element"><a href="#introduction">Introduction</a></li>
         <li className="navbar-element"><a id="navbar-about-me" href="#about-me">About Me</a></li>
         <li className="navbar-element"><a id="navbar-projects" href="#projects">Projects</a></li>
