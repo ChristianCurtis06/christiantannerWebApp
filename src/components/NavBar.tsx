1cc557270075300a67674582dd09d28cf1003eeb
ChristianCurtis06 <ctcurtis10@gmail.com>
2025-04-10 22:06:04 -0500

    Updated styling for responsive design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import InteractionContext from "../context/InteractionContext";
 
 const NavBar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   const { interaction } = useContext(InteractionContext);
 
   return (
-    <nav>
-
-      <ul id="navbar" className={interaction.isScrolled ? "scrolled" : ""}>
-        <img src="CC_Signature-white.png" alt="Christian Tanner Favicon" id="navbar-favicon" onClick={() => window.location.href = "/"} />
+    <nav id="navbar">
+      <button id="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <ul className={`navbar-elements ${isMenuOpen ? "open" : ""} ${interaction.isScrolled ? "scrolled" : ""}`}>
+        <img id="navbar-favicon" src="CC_Signature-white.png" alt="Christian Tanner Favicon" onClick={() => window.location.href = "/"} />
         <li className="navbar-element"><a href="#introduction">Introduction</a></li>
         <li className="navbar-element"><a id="navbar-about-me" href="#about-me">About Me</a></li>
         <li className="navbar-element"><a id="navbar-projects" href="#projects">Projects</a></li>
